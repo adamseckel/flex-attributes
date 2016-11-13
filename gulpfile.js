907e43a27cac879c713f8910871e70cb0b24ab94
aseckel <adam.seckel@gmail.com>
2016-11-13 13:36:14 -0800

    update compile, autoprefix

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
 
 // Get one .styl file and render
-gulp.task('build', function () {
+gulp.task('default', function () {
+  var processors = [
+    autoprefixer(),
+    // autoprefixer({browsers: ['last 1 versions']}),
+    cssnano(),
+  ];
   return gulp.src('./src/flex-attributes.styl')
     .pipe(stylus())
+    .pipe(postcss(processors))
     .pipe(gulp.dest('./'));
 });
